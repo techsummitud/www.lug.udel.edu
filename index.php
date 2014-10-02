05241c4b3697d7ea592b71d8e3243a51ec1db47d
guitarninja2 <Jacobaimino@gmail.com>
2014-10-01 21:23:43 -0400

    Ooops

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php include "header.php" ?>
+        <title>LUG@UD - Home</title>
+    </head>
+    <body>
+        <!-- Nav bar -->
+        <?php include 'navbar.php'; ?>
+        <div style="padding-top:115px;"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                </div>
+            </div>
+        </div>
+        <div style="padding-top:100px;"></div>
+        <?php include 'footer.php'; ?>
+
+
+        <!-- Bootstrap core JavaScript
+        ================================================== -->
+        <!-- Placed at the end of the document so the pages load faster -->
+        <script src="/assets/js/jquery.min.js"></script>
+        <script src="/assets/js/bootstrap.min.js"></script>
+    </body>
+</html>
