f708fe5adef7d4926a58601732e0f572dc4db52d
Byron Lambrou <byronl@udel.edu>
2016-02-06 14:07:26 -0500

    misspelled my name because I am a dumb person

--- members.php
+++ members.php
@@ -52,7 +52,7 @@
                                     </p>
                                 </div>
                                 <div class="col-md-4">
-                                    <img class="img img-responsive img-center callout" src="/assets/img/Byron_460x460.jpg">
+                                    <img class="img img-responsive img-center callout" src="/assets/img/byron_460x460.jpg">
                                     <h3 class="text-center">Byron Lambrou, <strong>Vice President</strong></h3>
                                     <p class="text-center">
                                         The Vice President of Membership organizes the mailing list, and keeps track of new members. 
